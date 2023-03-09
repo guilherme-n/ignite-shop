a2725ff354d218fa24c5048c11012a72ab72f8ad
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-03-09 12:32:36 -0300

    create global css, load google fonts and setup stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ export default function Home() {
 				<title>Ignite Shop</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
-			<main>Hello world</main>
 		</>
 	);
 }
