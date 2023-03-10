d12be0d7ee5eb54cd4d40c7831cfab4f136fa503
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-03-09 21:25:42 -0300

    create homepage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
-import Head from 'next/head';
+import Image from 'next/image';
+import { HomeContainer, Product } from '../styles/pages/home';
+
+import tShirt1 from '../assets/t-shirts/1.png';
+import tShirt2 from '../assets/t-shirts/2.png';
+import tShirt3 from '../assets/t-shirts/3.png';
 
 export default function Home() {
 	return (
-		<>
-			<Head>
-				<title>Ignite Shop</title>
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-			</Head>
-		</>
+		<HomeContainer>
+			<Product>
+				<Image src={tShirt1} width={520} height={480} alt='T Shirt' />
+				<footer>
+					<strong>T Shirt 1</strong>
+					<span>$19.90</span>
+				</footer>
+			</Product>
+			<Product>
+				<Image src={tShirt2} width={520} height={480} alt='T Shirt' />
+				<footer>
+					<strong>T Shirt 2</strong>
+					<span>$19.90</span>
+				</footer>
+			</Product>
+		</HomeContainer>
 	);
 }
